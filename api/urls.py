0665e76253f9468c80f95d7327abe776a0f72abc
Lierni.Ortiz <lierni.ortiz@versia.com>
2022-03-21 14:40:01 +0100

    hirugarren igoera

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoomView
+from .views import RoomView, CreateRoomView, GetRoomView, JoinRoom, UserInRoom, LeaveRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
-    path('get-room', GetRoomView.as_view())
+    path('get-room', GetRoomView.as_view()),
+    path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view()),
+    path('leave-room', LeaveRoom.as_view())
+
 ]
